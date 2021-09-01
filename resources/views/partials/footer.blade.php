523d785fefab228b362bc90f8f866c0eb4a9a5d8
EdwardKingAlexander <Edward@RockyMountainWeb.Design>
2021-09-01 14:25:33 -0600

    adjusted footer links

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,40 +3,29 @@
   <div class="px-4 py-12 mx-auto overflow-hidden max-w-7xl sm:px-6 lg:px-8">
     <nav class="flex flex-wrap justify-center -mx-5 -my-2" aria-label="Footer">
       <div class="px-5 py-2">
-        <a href="#" class="text-base text-white hover:text-gray-900">
+        <a href="#about-me" class="text-base text-white hover:text-gray-900">
           About
         </a>
       </div>
 
       <div class="px-5 py-2">
-        <a href="#" class="text-base text-white hover:text-gray-900">
-          Blog
+        <a href="#portfolio" class="text-base text-white hover:text-gray-900">
+          Portfolio
         </a>
       </div>
 
       <div class="px-5 py-2">
-        <a href="#" class="text-base text-white hover:text-gray-900">
-          Jobs
+        <a href="#services" class="text-base text-white hover:text-gray-900">
+          Services
         </a>
       </div>
 
       <div class="px-5 py-2">
-        <a href="#" class="text-base text-white hover:text-gray-900">
-          Press
+        <a href="#contact" class="text-base text-white hover:text-gray-900">
+          Contact
         </a>
       </div>
-
-      <div class="px-5 py-2">
-        <a href="#" class="text-base text-white hover:text-gray-900">
-          Accessibility
-        </a>
-      </div>
-
-      <div class="px-5 py-2">
-        <a href="#" class="text-base text-white hover:text-gray-900">
-          Partners
-        </a>
-      </div>
+      
     </nav>
     <div class="flex justify-center mt-8 space-x-6">
       <a href="https://www.facebook.com/Poppytheballoontwister" class="text-white hover:text-gray-900">
