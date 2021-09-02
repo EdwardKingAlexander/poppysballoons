c46919af02b478bb5cc3f8faa253cc129bc01e9f
EdwardKingAlexander <Edward@RockyMountainWeb.Design>
2021-09-01 22:35:16 -0600

    fixed mobile navigation links

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -87,7 +87,7 @@ class="sticky top-0 bg-primary">
                 >
                   <button   
                   type="button" 
-                  class="inline-flex items-center justify-center p-2 text-white rounded-md bg-primary hover: hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  class="inline-flex items-center justify-center p-2 text-white rounded-md bg-primary hover:text-black hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                     <span class="sr-only">{{ __('lang.close-menu') }}</span>
 
                     <!-- Heroicon name: outline/x -->
@@ -110,13 +110,13 @@ class="sticky top-0 bg-primary">
                     </div>
                   </a>
   
-                  <a href="#about-me" class="flex items-center p-3 -m-3 text-white rounded-lg hover:bg-gray-50">
+                  <a href="#portfolio" class="flex items-center p-3 -m-3 text-white rounded-lg hover:bg-gray-50">
                     <div class="ml-4 text-lg font-medium text-white hover:text-black">
                       {{ __('lang.nav-link-2') }}
                     </div>
                   </a>
   
-                  <a href="#contact" class="flex items-center p-3 -m-3 rounded-lg hover:bg-gray-50">
+                  <a href="#about-me" class="flex items-center p-3 -m-3 rounded-lg hover:bg-gray-50">
                     <div class="ml-4 text-lg font-medium text-white hover:text-black">
                       {{ __('lang.nav-link-3') }}
                     </div>
@@ -129,7 +129,7 @@ class="sticky top-0 bg-primary">
                   </a>
   
                   
-                  <a href="" class="">
+                  <a href="tel:7193547012" class="">
                     <span class="inline-flex items-center p-3 -m-3 rounded-lg hover:bg-gray-50">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="white">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" /> 
